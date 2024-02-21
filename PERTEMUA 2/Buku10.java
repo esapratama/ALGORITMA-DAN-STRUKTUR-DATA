d50ab965271c27943a3c8160e38dd6c03d1ec67e
esapratama <ptrprtm16@gmail.com>
2024-02-21 12:45:02 +0700

    first commit

---
+++ PERTEMUA 2/Buku10.java
@@ -0,0 +1,26 @@
+public class Buku10 {
+
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("pengarang: " + pengarang);
+        System.out.println("Jumlah halaman: " + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: Rp " + harga);
+    }
+
+    void terjual(int jml) {
+        stok -= jml;
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHargastock(int hrg) {
+        harga = hrg;
+    }
+
+}
