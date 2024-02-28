c3cf8c1bb7d9d3e7781d3fc891869c6a052e4fa6
esapratama <ptrprtm16@gmail.com>
2024-02-28 15:18:34 +0700

    Latihan Praktikum

--- PERTEMUAN 3/Kerucut10.java
+++ PERTEMUAN 3/Kerucut10.java
@@ -1,17 +1,3 @@
 public class Kerucut10 {
 
-    public int sisi;
-    public int rusuk;
-    public int titikSudut;
-
-    public Kerucut10(int s, int r, int t) {
-        sisi = s;
-        rusuk = r;
-        titikSudut = t;
-    }
-
-    public int hitungVolume() {
-        return sisi * rusuk * titikSudut;
-    }
-
 }
